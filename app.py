d17a019d9aefb7acc90761fc99d525302e3f2b31
nitin85058 <nitinjai85058@gmail.com>
2025-01-25 19:20:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 import os
+HF_TOKEN="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"
 
 from dotenv import load_dotenv
 load_dotenv()
